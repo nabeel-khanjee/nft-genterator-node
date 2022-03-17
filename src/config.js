65b4a81d4fbb2667e8090fa10b291eae696bf807
Nabeel Khanjee <nabeelshakee966@gmail.com>
2022-03-18 00:51:32 +0500

    second character

--- src/config.js
+++ src/config.js
@@ -1,17 +1,17 @@
 const layersOrder = [
-    { name: 'background', number: 1 },
-    { name: 'ball', number: 2 },
-    { name: 'eye color', number: 12 },
-    { name: 'iris', number: 3 },
-    { name: 'shine', number: 1 },
-    { name: 'shine', number: 1 },
-    { name: 'bottom lid', number: 3 },
-    { name: 'top lid', number: 3 },
+    { name: 'background', number: 5 },
+    { name: 'character', number: 2 },
+    { name: 'shirts', number: 8 },
+    { name: 'glasses', number: 8 },
+    // { name: 'shine', number: 1 },
+    { name: 'cig', number: 2 },
+    { name: 'chain', number: 3 },
+    // { name: 'top lid', number: 3 },
 ];
   
 const format = {
-    width: 230,
-    height: 230
+    width: 2000,
+    height: 2000
 };
 
 const rarity = [
@@ -20,6 +20,6 @@ const rarity = [
     { key: "_sr", val: "super rare" },
 ];
 
-const defaultEdition = 10;
+const defaultEdition = 50;
 
 export  { layersOrder, format, rarity, defaultEdition };
